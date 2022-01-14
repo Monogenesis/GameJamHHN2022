c66348d21f5e8e5c040b61390eba35557eae1d43
Amadeus Zittel <ama.zittel@gmail.com>
2022-01-14 17:09:09 +0100

    second commit

---
+++ Assets/Scripts/UIToolkitConvenience/SimpleUIInteractionMediator.cs
@@ -0,0 +1,210 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+namespace UIToolkitConvenience
+{
+    public class SimpleUIInteractionMediator : MonoBehaviour
+    {
+        [SerializeField] private UIDocument menuUIDocument;
+
+        #region InteractionLists
+
+        [SerializeField] private List<UIEventHandle<object>> buttonInteractions = new List<UIEventHandle<object>>();
+
+        [SerializeField] private List<UIEventHandle<ChangeEvent<string>>> dropdownInteractions =
+            new List<UIEventHandle<ChangeEvent<string>>>();
+
+        [SerializeField] private List<UIEventHandle<ChangeEvent<string>>> textFieldInteractions =
+            new List<UIEventHandle<ChangeEvent<string>>>();
+
+        [SerializeField] private List<UIEventHandle<ChangeEvent<float>>> floatSliderInteractions =
+            new List<UIEventHandle<ChangeEvent<float>>>();
+
+        [SerializeField] private List<UIEventHandle<ChangeEvent<int>>> intSliderInteractions =
+            new List<UIEventHandle<ChangeEvent<int>>>();
+
+        [SerializeField] private List<UIEventHandle<ChangeEvent<Vector2>>> minMaxSliderInteractions =
+            new List<UIEventHandle<ChangeEvent<Vector2>>>();
+
+        [SerializeField] private List<UIEventHandle<ChangeEvent<bool>>> radioButtonInteractions =
+            new List<UIEventHandle<ChangeEvent<bool>>>();
+
+        [SerializeField] private List<UIEventHandle<ChangeEvent<int>>> radioButtonGroupInteractions =
+            new List<UIEventHandle<ChangeEvent<int>>>();
+
+        [SerializeField] private List<UIEventHandle<ChangeEvent<bool>>>
+            toggleInteractions = new List<UIEventHandle<ChangeEvent<bool>>>();
+
+        [SerializeField] private List<UIEventHandle<ChangeEvent<bool>>>
+            foldoutInteractions = new List<UIEventHandle<ChangeEvent<bool>>>();
+
+        #endregion
+
+        #region Properties
+
+        public List<UIEventHandle<object>> ButtonInteractions => buttonInteractions;
+        public List<UIEventHandle<ChangeEvent<bool>>> ToggleInteractions => toggleInteractions;
+        public List<UIEventHandle<ChangeEvent<string>>> DropdownInteractions => dropdownInteractions;
+        public List<UIEventHandle<ChangeEvent<string>>> TextFieldInteractions => textFieldInteractions;
+        public List<UIEventHandle<ChangeEvent<float>>> FloatSliderInteractions => floatSliderInteractions;
+        public List<UIEventHandle<ChangeEvent<int>>> INTSliderInteractions => intSliderInteractions;
+        public List<UIEventHandle<ChangeEvent<Vector2>>> MinMaxSliderInteractions => minMaxSliderInteractions;
+        public List<UIEventHandle<ChangeEvent<bool>>> RadioButtonInteractions => radioButtonInteractions;
+        public List<UIEventHandle<ChangeEvent<int>>> RadioButtonGroupInteractions => radioButtonGroupInteractions;
+        public List<UIEventHandle<ChangeEvent<bool>>> FoldoutInteractions => foldoutInteractions;
+
+        #endregion
+
+        private VisualElement _menuRoot;
+
+        // private  List<string> _duplicateNameReferenceTypeNames = new List<string>();
+        private readonly HashSet<string> _duplicateNameReferenceTypeNames = new();
+        public HashSet<string> DuplicateNameReferenceTypeNames => _duplicateNameReferenceTypeNames;
+
+        private readonly HashSet<string> _missingEventReferenceTypeNames = new();
+        public HashSet<string> MissingEventReferenceTypeNames => _missingEventReferenceTypeNames;
+
+        private readonly HashSet<string> _emptyNameReferenceTypeNames = new();
+        public HashSet<string> EmptyNameReferenceTypeNames => _emptyNameReferenceTypeNames;
+
+        private void OnValidate()
+        {
+            _duplicateNameReferenceTypeNames.Clear();
+            _emptyNameReferenceTypeNames.Clear();
+            menuUIDocument ??= GetComponent<UIDocument>();
+            _menuRoot = menuUIDocument != null ? menuUIDocument.rootVisualElement : null;
+
+            if (_menuRoot is not null)
+            {
+                buttonInteractions = LoadElements<Button>(_menuRoot, buttonInteractions);
+                dropdownInteractions =
+                    LoadElements<DropdownField, ChangeEvent<string>, string>(_menuRoot, dropdownInteractions);
+
+                textFieldInteractions =
+                    LoadElements<TextField, ChangeEvent<string>, string>(_menuRoot, textFieldInteractions);
+
+                floatSliderInteractions =
+                    LoadElements<Slider, ChangeEvent<float>, float>(_menuRoot, floatSliderInteractions);
+
+                intSliderInteractions =
+                    LoadElements<SliderInt, ChangeEvent<int>, int>(_menuRoot, intSliderInteractions);
+
+                radioButtonInteractions =
+                    LoadElements<RadioButton, ChangeEvent<bool>, bool>(_menuRoot, radioButtonInteractions);
+
+                radioButtonGroupInteractions =
+                    LoadElements<RadioButtonGroup, ChangeEvent<int>, int>(_menuRoot, radioButtonGroupInteractions);
+
+                toggleInteractions = LoadElements<Toggle, ChangeEvent<bool>, bool>(_menuRoot, toggleInteractions);
+
+                minMaxSliderInteractions =
+                    LoadElements<MinMaxSlider, ChangeEvent<Vector2>, Vector2>(_menuRoot, minMaxSliderInteractions);
+
+                foldoutInteractions = LoadElements<Foldout, ChangeEvent<bool>, bool>(_menuRoot, foldoutInteractions);
+            }
+        }
+
+        public void OnRefreshButtonPressed()
+        {
+            OnValidate();
+        }
+
+        private List<UIEventHandle<TEventReturnType>> LoadElements<TElementType, TEventReturnType, TEventType>(
+            VisualElement root,
+            List<UIEventHandle<TEventReturnType>> interactionList)
+            where TElementType : VisualElement, INotifyValueChanged<TEventType>
+            where TEventReturnType : ChangeEvent<TEventType>
+        {
+            var elementList = root.Query<TElementType>();
+            elementList.ForEach(
+                type =>
+                {
+                    UIEventHandle<TEventReturnType> handle = new UIEventHandle<TEventReturnType>(type.name,
+                        $"ChangeEvent<{typeof(TEventReturnType).GetGenericArguments()[0].Name}>");
+                    interactionList.Add(handle);
+                    type.RegisterValueChangedCallback(evt =>
+                    {
+                        var result = interactionList.First(ele => ele.Name == type.name);
+                        result.InteractionEvent.Invoke((TEventReturnType) evt);
+                    });
+                });
+
+            return FilterObsoleteElements<TElementType, TEventReturnType>(interactionList,
+                new List<VisualElement>(elementList.ToList()));
+        }
+
+        private List<UIEventHandle<object>> LoadElements<TElementType>(VisualElement root,
+            List<UIEventHandle<object>> interactionList) where TElementType : Button
+        {
+            var elementList = root.Query<TElementType>();
+            elementList.ForEach(
+                type =>
+                {
+                    UIEventHandle<object> handle = new UIEventHandle<object>(type.name, "None");
+                    interactionList.Add(handle);
+                    type.clicked += () =>
+                    {
+                        interactionList.First(ele => ele.Name == type.name).InteractionEvent.Invoke(null);
+                    };
+                });
+
+            return FilterObsoleteElements<Button, object>(interactionList,
+                new List<VisualElement>(elementList.ToList()));
+        }
+
+
+        private List<UIEventHandle<TEventType>> FilterObsoleteElements<TElementType, TEventType>(
+            List<UIEventHandle<TEventType>> list,
+            List<VisualElement> elements)
+        {
+            // Remove old elements
+            list.RemoveAll(handle => elements.ToList().All(type => type.name != handle.Name));
+
+            bool hasDuplicate = list.GroupBy(handle => handle.Name).Distinct().Count() < list.Count;
+
+            if (hasDuplicate)
+            {
+                _duplicateNameReferenceTypeNames.Add(typeof(TElementType).Name);
+            }
+
+            if (list.ToList().Any(handle => handle.Name.Equals("")))
+            {
+                _emptyNameReferenceTypeNames.Add(typeof(TElementType).Name);
+            }
+
+            return list.GroupBy(handle => handle.Name).Select(handles => handles.First()).ToList();
+        }
+
+        [Serializable]
+        public class UIEventHandle<T>
+        {
+            [SerializeField, ReadOnly] private string name;
+            [SerializeField, ReadOnly] private string eventParameterType;
+            [SerializeField] private UnityEvent<T> interactionEvent;
+
+            public string Name
+            {
+                get => name;
+                set => name = value;
+            }
+
+            public UnityEvent<T> InteractionEvent
+            {
+                get => interactionEvent;
+                set => interactionEvent = value;
+            }
+
+            public UIEventHandle(string name, string eventTypeName)
+            {
+                this.name = name;
+                eventParameterType = eventTypeName;
+                interactionEvent = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIToolkitConvenience/SimpleUIInteractionMediatorInspector.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+using static UIToolkitConvenience.SimpleUIInteractionMediator;
+
+namespace UIToolkitConvenience
+{
+    [CustomEditor(typeof(SimpleUIInteractionMediator))]
+    public class SimpleUIInteractionMediatorInspector : Editor
+    {
+        private readonly HashSet<string> _missingEventReferenceTypeNames = new();
+
+        private static readonly List<string> ListNames = new()
+        {
+            "buttonInteractions",
+            "dropdownInteractions",
+            "textFieldInteractions",
+            "floatSliderInteractions",
+            "intSliderInteractions",
+            "minMaxSliderInteractions",
+            "radioButtonInteractions",
+            "radioButtonGroupInteractions",
+            "toggleInteractions",
+            "foldoutInteractions"
+        };
+
+        public override void OnInspectorGUI()
+        {
+            SerializedProperty property;
+            _missingEventReferenceTypeNames.Clear();
+            serializedObject.Update();
+            SimpleUIInteractionMediator myScript = (SimpleUIInteractionMediator) target;
+            EditorGUILayout.ObjectField("Script:", MonoScript.FromMonoBehaviour((SimpleUIInteractionMediator) target),
+                typeof(SimpleUIInteractionMediator), false);
+
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("menuUIDocument"));
+
+            if (GUILayout.Button("REFRESH"))
+            {
+                myScript.OnRefreshButtonPressed();
+                Debug.Log("Finished Parsing The UIDocument!");
+            }
+
+            CheckForMissingReference<UIEventHandle<object>, object>(myScript.ButtonInteractions, ListNames[0]);
+
+            CheckForMissingReference<UIEventHandle<ChangeEvent<string>>, ChangeEvent<string>>(
+                myScript.DropdownInteractions, ListNames[1]);
+
+            CheckForMissingReference<UIEventHandle<ChangeEvent<string>>, ChangeEvent<string>>(
+                myScript.TextFieldInteractions, ListNames[2]);
+
+            CheckForMissingReference<UIEventHandle<ChangeEvent<float>>, ChangeEvent<float>>(
+                myScript.FloatSliderInteractions, ListNames[3]);
+
+            CheckForMissingReference<UIEventHandle<ChangeEvent<int>>, ChangeEvent<int>>(
+                myScript.INTSliderInteractions, ListNames[4]);
+
+            CheckForMissingReference<UIEventHandle<ChangeEvent<Vector2>>, ChangeEvent<Vector2>>(
+                myScript.MinMaxSliderInteractions, ListNames[5]);
+
+            CheckForMissingReference<UIEventHandle<ChangeEvent<bool>>, ChangeEvent<bool>>(
+                myScript.RadioButtonInteractions, ListNames[6]);
+
+            CheckForMissingReference<UIEventHandle<ChangeEvent<int>>, ChangeEvent<int>>(
+                myScript.RadioButtonGroupInteractions, ListNames[7]);
+
+            CheckForMissingReference<UIEventHandle<ChangeEvent<bool>>, ChangeEvent<bool>>(
+                myScript.ToggleInteractions, ListNames[8]);
+
+            CheckForMissingReference<UIEventHandle<ChangeEvent<bool>>, ChangeEvent<bool>>(
+                myScript.FoldoutInteractions, ListNames[9]);
+
+
+            if (myScript.DuplicateNameReferenceTypeNames.Count > 0)
+            {
+                EditorGUILayout.HelpBox(
+                    $"ACTION REQUIRED - DUPLICATE NAMES: {string.Join(", ", myScript.DuplicateNameReferenceTypeNames)}",
+                    MessageType.Error);
+            }
+
+            if (myScript.EmptyNameReferenceTypeNames.Count > 0)
+            {
+                EditorGUILayout.HelpBox(
+                    $"ACTION REQUIRED - EMPTY NAMES: {string.Join(", ", myScript.EmptyNameReferenceTypeNames)}",
+                    MessageType.Error);
+            }
+
+            if (_missingEventReferenceTypeNames.Count > 0)
+            {
+                EditorGUILayout.HelpBox(
+                    $"Missing Event References For: {string.Join(", ", _missingEventReferenceTypeNames)}",
+                    MessageType.Warning);
+            }
+
+
+            foreach (var listName in ListNames)
+            {
+                property = serializedObject.FindProperty(listName);
+                CheckForVisibility(property);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void ForListDo<TListType, TDataType>(List<TListType> interactionList, string typeName, Action action)
+            where TListType : UIEventHandle<TDataType>
+        {
+            action.Invoke();
+        }
+
+        private void CheckForMissingReference<TListType, TDataType>(List<TListType> interactionList, string typeName)
+            where TListType : UIEventHandle<TDataType>
+        {
+            bool missingRefFound = false;
+            for (int i = 0; i < interactionList.Count; i++)
+            {
+                var eventHandler = interactionList[i];
+                if (eventHandler.InteractionEvent?.GetPersistentEventCount() < 1)
+                {
+                    missingRefFound = true;
+                }
+                else
+                {
+                    for (int j = 0; j < eventHandler.InteractionEvent?.GetPersistentEventCount(); j++)
+                    {
+                        if (eventHandler.InteractionEvent.GetPersistentMethodName(j).Equals(""))
+                        {
+                            missingRefFound = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (missingRefFound)
+                {
+                    _missingEventReferenceTypeNames.Add(typeName);
+                    break;
+                }
+            }
+        }
+
+        private void CheckForVisibility(SerializedProperty property)
+        {
+            if (property.arraySize > 0)
+            {
+                EditorGUILayout.PropertyField(property, true);
+            }
+        }
+    }
+}
